e11447655096efe0d8e2f7b0d506b2a4c5537101
Ryan Cole <ryancole@unt.edu>
2011-07-24 00:46:14 -0500

    ok, pypi package works this time.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-import os
-from setuptools import setup, find_packages
+import os, setuptools
 
 README = os.path.join(os.path.dirname(__file__),'README.rst')
-long_description = open(README).read() + '\n'
 
-setup(name='toaster',
-      version='0.0.1',
-      description='A static blog generator inspired by Jekyll.',
-      long_description=long_description,
-      classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.6',
-      ],
-      keywords='toaster pip easy_install distutils setuptools',
-      author='Ryan Cole',
-      author_email='ryan@rycole.com',
-      url='https://github.com/ryancole/toaster',
-      license='MIT',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False)
+setuptools.setup(
+    name = 'toaster',
+    version = '0.0.1',
+    packages = setuptools.find_packages(),
+    scripts = ['bin/toast'],
+    install_requires = ['pyyaml', 'markdown', 'jinja2'],
+    
+    # explicity include readme
+    package_data = {'': ['README.rst']},
+    
+    # meta-data for upload to pypi
+    author = 'Ryan Cole',
+    author_email = 'ryan@rycole.com',
+    description = 'A static blog generator inspired by Jekyll.',
+    license = 'MIT',
+    keywords = 'toaster static blog jekyll',
+    url = 'https://github.com/ryancole/toaster',
+    include_package_data = True,
+)
