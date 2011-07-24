e5c3f7e0820d543cc3768a8392c3decc2fad12d7
Ryan Cole <ryancole@unt.edu>
2011-07-23 19:30:44 -0500

    modified pypi version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='toaster',
-    version='0.0.1',
+    version='0.0.2',
     author='Ryan Cole',
     author_email='ryan@rycole.com',
     packages=['toaster'],
-    scripts=['bin/toaster.py'],
+    scripts=['bin/toast.py'],
     url='http://pypi.python.org/pypi/toaster/',
     description='A static blog generator inspired by Jekyll.',
     long_description=open('readme.md').read(),
