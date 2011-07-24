7726e2251c3cac8f56905481b9927c4ff8968b0d
Ryan Cole <ryancole@unt.edu>
2011-07-23 23:29:35 -0500

    renamed readme for pypi.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='toaster',
-    version='0.0.2',
+    version='0.0.1',
     author='Ryan Cole',
     author_email='ryan@rycole.com',
     packages=['toaster'],
@@ -10,4 +10,5 @@ setup(
     url='http://pypi.python.org/pypi/toaster/',
     description='A static blog generator inspired by Jekyll.',
     long_description=open('readme.md').read(),
+    requires=['pyyaml', 'jinja2', 'markdown']
 )
